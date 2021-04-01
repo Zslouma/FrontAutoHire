4344377e8347453a69aba81603f57180ac7aaad0
Aymen Smati <ayman@Smatis-MacBook-Pro.local>
2021-04-01 13:34:39 +0100

    Header is Okay

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,5 +1,5 @@
 class Utils {
-  static String url = "localhost:3000";
+  static String url = "192.168.43.105:3000";
   static String company = "/entreprise";
   static String candidat = "/candidature/";
   static String test = "/test/";
@@ -7,5 +7,4 @@ class Utils {
   static String offre = "/offre/All/Offre";
   static String user = "/user/";
   static String evaluation = "/evaluation";
- 
 }
